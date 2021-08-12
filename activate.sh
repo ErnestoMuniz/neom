aadca8e43204e8e9e0c90509cb2a265f8a8a2493
Ernesto Muniz <ernestotrapia@gmail.com>
2021-08-12 14:41:16 -0300

    Fixed Datacom verification bug

---
+++ activate.sh
@@ -0,0 +1,2 @@
+rm vendor/backpack/crud/src/LicenseCheck.php
+cp public/LicenseCheck.php vendor/backpack/crud/src/
--- app/Http/Controllers/DatacomController.php
+++ app/Http/Controllers/DatacomController.php
@@ -33,7 +33,7 @@ class DatacomController extends Controller
 
     public static function cpu($id){
         $olt = DB::table('olts')->where('id', $id)->first();
-        $output = shell_exec("python python/datacom/datacom_cpu.py $olt->ip $olt->user $olt->pass");
+        $output = shell_exec("python python/datacom/datacom_cpu.py '$olt->ip' '$olt->user' '$olt->pass'");
         $output = json_decode($output, true);
         $cpu = $output['data']['dmos-base:status']['system']['dmos-system-monitor:cpu']['chassis'][0]['slot'][0]['load']['five-seconds']['active'];
         DB::table('olts')->where('id', $id)->update(['last_cpu' => $cpu]);
@@ -42,7 +42,7 @@ class DatacomController extends Controller
 
     public static function mem($id){
         $olt = DB::table('olts')->where('id', $id)->first();
-        $output = shell_exec("python python/datacom/datacom_mem.py $olt->ip $olt->user $olt->pass");
+        $output = shell_exec("python python/datacom/datacom_mem.py '$olt->ip' '$olt->user' '$olt->pass'");
         $output = json_decode($output, true);
         $total = $output['data']['dmos-base:status']['system']['dmos-system-monitor:memory']['chassis'][0]['slot'][0]['five-seconds']['total'];
         $used = $output['data']['dmos-base:status']['system']['dmos-system-monitor:memory']['chassis'][0]['slot'][0]['five-seconds']['used'];
@@ -53,7 +53,7 @@ class DatacomController extends Controller
 
     public static function firmware($id){
         $olt = DB::table('olts')->where('id', $id)->first();
-        $output = shell_exec("python python/datacom/datacom_firmware.py $olt->ip $olt->user $olt->pass");
+        $output = shell_exec("python python/datacom/datacom_firmware.py '$olt->ip' '$olt->user' '$olt->pass'");
         $output = json_decode($output, true);
         $firmware = $output['data']['dmos-base:status']['firmware']['dmos-sw-update:firmware'][0]['version'];
         DB::table('olts')->where('id', $id)->update(['firmware' => $firmware]);
@@ -62,13 +62,13 @@ class DatacomController extends Controller
 
     public static function pon($id, $cmd){
         $olt = DB::table('olts')->where('id', $id)->first();
-        $output = shell_exec("python python/datacom/datacom_count.py $olt->ip $olt->user $olt->pass $cmd");
+        $output = shell_exec("python python/datacom/datacom_count.py '$olt->ip' '$olt->user' '$olt->pass' '$cmd'");
         echo $output;
     }
 
     public static function onu($id, $cmd){
         $olt = DB::table('olts')->where('id', $id)->first();
-        $output = shell_exec("python python/datacom/datacom_search.py $olt->ip $olt->user $olt->pass $cmd");
+        $output = shell_exec("python python/datacom/datacom_search.py '$olt->ip' '$olt->user' '$olt->pass' '$cmd'");
         $output = explode(' ', $output);
         echo $output[0] . '/' . $output[5];
     }
