6ac92a03422f404b696ad82c4e482abcb9720c77
Ernesto Muniz <ernestotrapia@gmail.com>
2021-08-23 15:57:45 -0300

    Version Alpha 2.8.2
    - Added enter trigger on search

--- resources/views/navigate.blade.php
+++ resources/views/navigate.blade.php
@@ -46,6 +46,20 @@
                         <button class="btn btn-primary" onclick="getONU({{ $olt->id }})" id="btn-search"><i class="las la-search"></i></button>
                     </div>
                 </div>
+                <script type="text/javascript">
+                var input = document.getElementById("search");
+
+                // Execute a function when the user releases a key on the keyboard
+                input.addEventListener("keyup", function(event) {
+                    // Number 13 is the "Enter" key on the keyboard
+                    if (event.keyCode === 13) {
+                        // Cancel the default action, if needed
+                        event.preventDefault();
+                        // Trigger the button element with a click
+                        document.getElementById("btn-search").click();
+                    }
+                });
+                </script>
             </div>
             <div style="height: 30em; overflow: auto;">
                 <style>
