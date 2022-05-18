a46ede83aca42cd1d3c0dcf031e385934c77116e
ErnestoMuniz <ernestotrapia@gmail.com>
2022-05-18 12:06:07 -0300

    :bug: Fix user deletion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,93 +9,93 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $users = User::addSelect([
-            'role_name' => Role::select('name')
-            ->whereColumn('role_id', 'roles.id')
-        ])->orderBy('name')->get();
-        return $users;
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $users = User::addSelect([
+      'role_name' => Role::select('name')
+        ->whereColumn('role_id', 'roles.id')
+    ])->orderBy('name')->get();
+    return $users;
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(Request $request)
-    {
-        try {
-            User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'role_id' => $request->role_id
-            ]);
-            return response()->json(['status' => '200', 'message' => 'User created']);
-        } catch (\Throwable $th) {
-            return response()->json(['status' => '500', 'message' => 'Error on user creation'], 500);
-        }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function store(Request $request)
+  {
+    try {
+      User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'role_id' => $request->role_id
+      ]);
+      return response()->json(['status' => '200', 'message' => 'User created']);
+    } catch (\Throwable $th) {
+      return response()->json(['status' => '500', 'message' => "Error on user creation\nErr: " . $th], 500);
     }
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        $user = User::addSelect([
-            'role_name' => Role::select('name')
-            ->whereColumn('role_id', 'roles.id')
-        ])->where('users.id', $user->id)->first();
-        return $user;
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\Response
+   */
+  public function show(User $user)
+  {
+    $user = User::addSelect([
+      'role_name' => Role::select('name')
+        ->whereColumn('role_id', 'roles.id')
+    ])->where('users.id', $user->id)->first();
+    return $user;
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function update(Request $request, User $user)
-    {
-        try {
-            $user = User::find($user->id);
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->role_id = $request->role_id;
-            if ($request->password != '') {
-                $user->password = Hash::make($request->password);
-            }
-            $user->save();
-            return response()->json(['status' => '200', 'message' => 'User updated']);
-        } catch (\Throwable $th) {
-            return response()->json(['status' => '500', 'message' => 'Error on user update'], 500);
-        }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function update(Request $request, User $user)
+  {
+    try {
+      $user = User::find($user->id);
+      $user->name = $request->name;
+      $user->email = $request->email;
+      $user->role_id = $request->role_id;
+      if ($request->password != '') {
+        $user->password = Hash::make($request->password);
+      }
+      $user->save();
+      return response()->json(['status' => '200', 'message' => 'User updated']);
+    } catch (\Throwable $th) {
+      return response()->json(['status' => '500', 'message' => "Error on user update\nErr: " . $th], 500);
     }
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function destroy(User $user)
-    {
-        try {
-            User::destroy($user->id);
-            return response()->json(['status' => '200', 'message' => 'User deleted']);
-        } catch (\Throwable $th) {
-            return response()->json(['status' => '500', 'message' => 'Error on user deletion'], 500);
-        }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function destroy(User $user)
+  {
+    try {
+      User::destroy($user->id);
+      return response()->json(['status' => '200', 'message' => 'User deleted']);
+    } catch (\Throwable $th) {
+      return response()->json(['status' => '500', 'message' => "Error on user deletion\nErr: " . $th], 500);
     }
+  }
 }
