cc5a682edd17e2bdfcc25e623ddbb2c9edb13754
ErnestoOnline <ernesto.muniz@online.net.br>
2022-11-22 16:22:06 -0300

    :bug: Fix Fiberhome scripts

--- public/python/fiberhome/FiberHome.py
+++ public/python/fiberhome/FiberHome.py
@@ -17,7 +17,7 @@ class FiberHome():
         tn.write(f"LOGIN:::CTAG::UN={self.user},PWD={self.password};\n".encode('ascii'))
         tn.read_until(b";")
 
-        tn.write(f"ADD-ONU::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid}:CTAG::AUTHTYPE=MAC,ONUID={serial_onu},ONUTYPE={modelo_onu},NAME={pppoe_cliente};\n".encode('ascii'))
+        tn.write(f"ADD-ONU::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid}:CTAG::AUTHTYPE=MAC,ONUID={serial_onu},ONUTYPE={modelo_onu},NAME={desc};\n".encode('ascii'))
         print(tn.read_until(b";").decode('ascii'))
 
         tn.write(f"SET-WANSERVICE::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid},ONUIDTYPE=MAC,ONUID={serial_onu}:CTAG::STATUS=1,MODE=2,CONNTYPE=2,VLAN={vlan},COS=7,QOS=1,NAT=1,IPMODE=3,PPPOEPROXY=2,PPPOEUSER={pppoe_cliente},PPPOEPASSWD={password},PPPOENAME=,PPPOEMODE=1,UPORT=0;\n".encode('ascii'))
@@ -48,7 +48,7 @@ class FiberHome():
 
         tn.close()
 
-    def Activate_Router_ZTE(self, ip_olt, slot_pon_onuid, serial_onu, modelo_onu, pppoe_cliente, vlan):
+    def Activate_Router_ZTE(self, ip_olt, slot_pon_onuid, serial_onu, modelo_onu, desc, vlan):
 
         tn = telnetlib.Telnet(host=self.host, port=self.port)
 
@@ -56,7 +56,7 @@ class FiberHome():
         tn.write(f"LOGIN:::CTAG::UN={self.user},PWD={self.password};\n".encode('ascii'))
         tn.read_until(b";")
 
-        tn.write(f"ADD-ONU::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid}:CTAG::AUTHTYPE=MAC,ONUID={serial_onu},ONUTYPE={modelo_onu},NAME={pppoe_cliente};\n".encode('ascii'))
+        tn.write(f"ADD-ONU::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid}:CTAG::AUTHTYPE=MAC,ONUID={serial_onu},ONUTYPE={modelo_onu},NAME={desc};\n".encode('ascii'))
         print(tn.read_until(b";").decode('ascii'))
 
         tn.write(f"CFG-VEIPSERVICE::OLTID={ip_olt},PONID=NA-NA-{slot_pon_onuid},ONUIDTYPE=MAC,ONUID={serial_onu},ONUPORT=NA-NA-NA-1:CTAG::ServiceId=1,CVLANID={vlan},CCOS=0,ServiceModelProfile=ONUZTE,ServiceType=NONE;\n".encode('ascii'))
