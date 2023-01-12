af5be86dd7a2c0462f0893f27ac260234455aa68
ErnestoMuniz <ernestotrapia@gmail.com>
2023-01-12 09:28:56 -0300

    :bug: Fix status many

--- public/python/nokia/isam_onu_status_many.py
+++ public/python/nokia/isam_onu_status_many.py
@@ -18,17 +18,21 @@ tn.write(b"environment inhibit-alarms\n")
 tn.read_until(b"#")
 for onu in onus:
     if onu != '':
-        tn.write(f"show equipment ont index sn:{onu} xml\n".encode('ascii'))
-        pos = tn.read_until(b"#").decode('ascii').split(
-            'type="Gpon::OntIndex">')[1].split('</info>')[0]
-        spl = pos.split('/')
-        pon = f"{spl[0]}/{spl[1]}/{spl[2]}/{spl[3]}"
-        tn.write(
-            f"show equipment ont status pon {pon} ont {pos} | match exact:1/1\n".encode('ascii'))
-        res = tn.read_until(b"#").decode('ascii').split('\r\n')
-        res.pop(0)
-        res.pop(0)
-        res.pop()
-        print(' '.join('\n'.join(res).split()))
+        try:
+            tn.write(
+                f"show equipment ont index sn:{onu} xml\n".encode('ascii'))
+            pos = tn.read_until(b"#").decode('ascii').split(
+                'type="Gpon::OntIndex">')[1].split('</info>')[0]
+            spl = pos.split('/')
+            pon = f"{spl[0]}/{spl[1]}/{spl[2]}/{spl[3]}"
+            tn.write(
+                f"show equipment ont status pon {pon} ont {pos} | match exact:1/1\n".encode('ascii'))
+            res = tn.read_until(b"#").decode('ascii').split('\r\n')
+            res.pop(0)
+            res.pop(0)
+            res.pop()
+            print(' '.join('\n'.join(res).split()))
+        except:
+            print("- - - - - - - - -")
     else:
         print("- - - - - - - - -")
