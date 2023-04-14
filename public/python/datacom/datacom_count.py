c9fca0d2c090fe4123436f0e1118c3c249513427
ErnestoMuniz <ernestotrapia@gmail.com>
2023-04-14 13:48:11 -0300

    :bug: Fix Datacom count script

--- public/python/datacom/datacom_count.py
+++ public/python/datacom/datacom_count.py
@@ -3,11 +3,17 @@ import paramiko, sys, re
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname=sys.argv[1].split(':')[0], port=sys.argv[1].split(':')[1], username=sys.argv[2], password=sys.argv[3])
-stdin, stdout, stderr = ssh.exec_command("show interface gpon 1/{} onu | display curly-braces | nomore".format(sys.argv[4]))
+stdin, stdout, stderr = ssh.exec_command(f"show interface gpon 1/{sys.argv[4]} onu | nomore | include /")
+res = stdout.read().decode()
+onus = res.split('\n')
+serials = []
+del onus[-1]
+for onu in onus:
+  serials.append(onu.split()[2])
+stdin, stdout, stderr = ssh.exec_command(f"show interface gpon 1/{sys.argv[4]} onu | display curly-braces | nomore")
 res = stdout.read().decode()
 res = ' '.join(res.split())
 pos = re.findall("(?<=onu ).*?(?= )", res)
-serials = re.findall("(?<=serial-number ).*?(?=;)", res)
 status = re.findall("(?<=primary-status ).*?(?=;)", res)
 signals = re.findall("(?<=rx-optical-pw ).*?(?=;)", res)
 for i in range(len(status)):
